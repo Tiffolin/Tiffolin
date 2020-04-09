310eb449ba6754dbd2548b76393faf0ddecf6dba
Tiffo <szuchinlin3@gmail.com>
2020-04-09 16:52:17 -0400

    Updates

---
+++ precache-manifest.555d806dba9ca69780436089c43c0be1.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09448cc914338a84ead1a80520897e38",
+    "url": "/tiffolin/index.html"
+  },
+  {
+    "revision": "d91e26a0518c3ce53117",
+    "url": "/tiffolin/static/css/main.a1790eae.chunk.css"
+  },
+  {
+    "revision": "e3fc315fbd777ee42e54",
+    "url": "/tiffolin/static/js/2.60893a21.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/tiffolin/static/js/2.60893a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91e26a0518c3ce53117",
+    "url": "/tiffolin/static/js/main.9033e29f.chunk.js"
+  },
+  {
+    "revision": "bbcc3b111737987a07ee",
+    "url": "/tiffolin/static/js/runtime-main.66dc5db5.js"
+  },
+  {
+    "revision": "89fb33f2827d423be4eb54c86b154173",
+    "url": "/tiffolin/static/media/Design1.89fb33f2.PNG"
+  },
+  {
+    "revision": "ca5b564b0a2fdb484d1b879e91be2044",
+    "url": "/tiffolin/static/media/Design2.ca5b564b.PNG"
+  },
+  {
+    "revision": "5ba465b8dd232a7e29389fbefacda022",
+    "url": "/tiffolin/static/media/Design3.5ba465b8.PNG"
+  },
+  {
+    "revision": "06d4b17cab2dbacb636a1ad12791315e",
+    "url": "/tiffolin/static/media/Design4.06d4b17c.PNG"
+  },
+  {
+    "revision": "c36bb36f8253af3c9cda2a35e44214a7",
+    "url": "/tiffolin/static/media/Design5.c36bb36f.PNG"
+  },
+  {
+    "revision": "5b8104a3a1d72e1488b8387226385660",
+    "url": "/tiffolin/static/media/domain.5b8104a3.png"
+  },
+  {
+    "revision": "78d57d3fb05f2042e967be668af6a9b1",
+    "url": "/tiffolin/static/media/email3.78d57d3f.png"
+  },
+  {
+    "revision": "d3f5289b1dc7f21f4a71afa126c781b4",
+    "url": "/tiffolin/static/media/email5.d3f5289b.png"
+  },
+  {
+    "revision": "a2b7b602b0ddbe68d78bd2437a682815",
+    "url": "/tiffolin/static/media/github3.a2b7b602.png"
+  },
+  {
+    "revision": "dd73cb2eb894734b73d519f156eafe0c",
+    "url": "/tiffolin/static/media/github5.dd73cb2e.png"
+  },
+  {
+    "revision": "c5e7053853eb7ecf849c441231b2909d",
+    "url": "/tiffolin/static/media/linked3.c5e70538.png"
+  },
+  {
+    "revision": "da805f4e0ca1e1787eecdb9d25501e9d",
+    "url": "/tiffolin/static/media/linked5.da805f4e.png"
+  },
+  {
+    "revision": "a9608dcb462e7f3f714857699c670764",
+    "url": "/tiffolin/static/media/logo.a9608dcb.png"
+  },
+  {
+    "revision": "af2961ae3dc26b7ae90f5d91420c371b",
+    "url": "/tiffolin/static/media/name.af2961ae.png"
+  },
+  {
+    "revision": "bc49542f70e0d43c8d620d49e7a6b5a6",
+    "url": "/tiffolin/static/media/profile2.bc49542f.png"
+  },
+  {
+    "revision": "1adfcd6c99219fcac4652ef8feb459f9",
+    "url": "/tiffolin/static/media/recipeRecite.1adfcd6c.PNG"
+  },
+  {
+    "revision": "30539f69bd153ae0cd666b502883fe6c",
+    "url": "/tiffolin/static/media/sb.30539f69.png"
+  },
+  {
+    "revision": "a14f58cf9384c8642a037f8b7c492b2a",
+    "url": "/tiffolin/static/media/splash4.a14f58cf.png"
+  },
+  {
+    "revision": "86a6f8028785067fe3567f25257fc47f",
+    "url": "/tiffolin/static/media/testyourmemory.86a6f802.PNG"
+  },
+  {
+    "revision": "1513cfe76ffb22128aa544d2edfd40c7",
+    "url": "/tiffolin/static/media/timeToGo.1513cfe7.png"
+  }
+]);
