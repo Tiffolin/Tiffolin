c257d6b4d88d5da251719163e985887402c99b67
Jason Harris Edwards <jasonharrisedwards@JCH-MacBook-Air.local>
2026-01-31 18:36:33 -0800

    Fix React 18 upgrade issues and restore site rendering

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React, { Component } from "react";
-import {BrowserRouter, Route} from "react-router-dom";
-import Splash from "./component/Splash";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
 import Navbar from "./component/Navbar";
+import Splash from "./component/Splash";
 import About from "./component/About";
 import Projects from "./component/projects";
 // import Skills from "./component/Skills";
 // import Contact from "./component/Contact";
 import Footer from "./component/Footer";
 
-class App extends Component{
-  render() {
-    return(
-      <div id='App'>
-        <BrowserRouter>        
-          <div>
-            <Route exact path="/" component={Splash} />
-            <Navbar />
-            <About />
-            <Projects />
-            <Route exact path="/About" component={About} />
-            <Route exact path="/Projects" component={Projects} />
-            {/* <Route exact path="/Skills" component={Skills} />
-            <Route exact path="/Contact" component={Contact} /> */}
-            <Footer/>
-          </div>
-      </BrowserRouter>   
-    </div>
-    
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      {/* Always visible layout */}
+      <Route exact path="/" component={Splash} />
+      <Navbar />
+
+      {/* Pages */}
+      
+      <Route exact path="/about" component={About} />
+      <About />
+      
+      <Route exact path="/projects" component={Projects} />
+      <Projects />
+      {/* <Route exact path="/skills" component={Skills} /> */}
+      {/* <Route exact path="/contact" component={Contact} /> */}
+
+      {/* Always visible layout */}
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
